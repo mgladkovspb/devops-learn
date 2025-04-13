f4d61e92035ff9a5655ec6779cf32b741def4e8f
Max G <mgladkov@abz-1.ru>
2025-04-14 00:07:51 +0300

    ohhh...

---
+++ lesson20/kind_create.sh
@@ -0,0 +1,31 @@
+#! /bin/env bash
+
+docker build -t webbooks ./build
+
+export DOCKER_HOST_IP="172.28.219.74"
+# export DOCKER_HOST_EXTERNAL_IP="51.250.78.64"
+export POD_SUBNET="10.240.0.0/16"
+export SERVICE_SUBNET="10.0.0.0/16"
+export VOLUME_HOST_PATH='/tmp/data/'
+export VOLUME_CONTAINER_PATH="/data"
+
+# create kind cluster
+envsubst < config.yaml | kind create cluster --config=-
+
+# # add external ip docker-machine into kubeconfig
+# echo "Edit kube config file..."
+# sed -i "s/$DOCKER_HOST_IP/$DOCKER_HOST_EXTERNAL_IP/g" ~/.kube/config
+
+# # get kubernetes cluster info
+# kubectl --insecure-skip-tls-verify cluster-info
+
+# install Nginx ingress controller
+echo "Install Ingress NGINX..."
+kubectl --insecure-skip-tls-verify apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+# install Calico
+echo "Install Calico CNI..."
+kubectl --insecure-skip-tls-verify apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/calico.yaml
+# watch kubectl --insecure-skip-tls-verify get pods -l k8s-app=calico-node -A
+
+kind load docker-image webbooks --name kind
